815c9b1d007210d000b60ea97c5c979afd47e3f5
joshualukejewson <joshuajewson@gmail.com>
2026-01-26 21:57:22 +1000

    added api call to google gemini and a test response.

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
-def main():
-    print("Hello from ai-agent-project!")
+import os
+from dotenv import load_dotenv
+from google import genai
 
-
-if __name__ == "__main__":
-    main()
+load_dotenv()
+api_key = os.environ.get("GEMINI_API_KEY")
+if not api_key:
+    raise RuntimeError("fail to load api key")
+client = genai.Client(api_key=api_key)
+response = client.models.generate_content(
+    model="gemini-2.5-flash",
+    contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.",
+)
+print(response.text)
